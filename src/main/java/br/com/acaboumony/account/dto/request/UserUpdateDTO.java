a9f400374fdd3d8350f7b1eb4b3c775039e0685c
Vinicius dos Santos Candeias <79974062+Candeias-Vinicius@users.noreply.github.com>
2023-11-09 14:03:19 -0300

    Exception na account

--- src/main/java/br/com/acaboumony/account/dto/request/UserUpdateDTO.java
+++ src/main/java/br/com/acaboumony/account/dto/request/UserUpdateDTO.java
@@ -1,8 +1,10 @@
 package br.com.acaboumony.account.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record UserUpdateDTO(UUID userId,
+public record UserUpdateDTO(@NotNull UUID userId,
                             String name,
                             String contact,
                             String email) {
--- src/main/java/br/com/acaboumony/account/service/UserService.java
+++ src/main/java/br/com/acaboumony/account/service/UserService.java
@@ -7,6 +7,7 @@ import br.com.acaboumony.account.dto.response.UserResDTO;
 import br.com.acaboumony.account.model.User;
 import br.com.acaboumony.account.repository.UserRepository;
 import br.com.acaboumony.util.GenericMapper;
+import jakarta.persistence.EntityExistsException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,8 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final GenericMapper <UserReqDTO, User> userReqMapper;
-    private final GenericMapper <User, UserResDTO> userResMapper;
+    private final GenericMapper<UserReqDTO, User> userReqMapper;
+    private final GenericMapper<User, UserResDTO> userResMapper;
 
     public UserService(UserRepository userRepository, GenericMapper<UserReqDTO, User> userReqMapper, GenericMapper<User, UserResDTO> userResMapper) {
         this.userRepository = userRepository;
@@ -28,7 +29,10 @@ public class UserService {
         this.userResMapper = userResMapper;
     }
 
-    public void createUser(UserReqDTO userReqDTO){
+    public void createUser(UserReqDTO userReqDTO) {
+        validateCpf(userReqDTO.getCpf());
+        validateEmail(userReqDTO.getEmail());
+
         User user = new User();
         BeanUtils.copyProperties(userReqDTO, user);
         userRepository.save(userReqMapper.mapDtoToModel(userReqDTO, User.class));
@@ -39,17 +43,19 @@ public class UserService {
         return user.getPassword().equals(password) ? user.getUserId() : null;
     }
 
-    public List<UserResDTO> listUsers(){
+    public List<UserResDTO> listUsers() {
         return userResMapper.listToList(userRepository.findAll(), UserResDTO.class);
     }
 
-    public void deleteUser(UUID userId){
+    public void deleteUser(UUID userId) {
         userRepository.deleteById(userId);
     }
 
     @Transactional
     public void updateUser(UserUpdateDTO userUpdateDTO) {
+        validateEmail(userUpdateDTO.email());
         User user = userRepository.findById(userUpdateDTO.userId()).orElseThrow(NoSuchElementException::new);
+
         if (userUpdateDTO.name() != null) {
             user.setName(userUpdateDTO.name());
         }
@@ -59,10 +65,35 @@ public class UserService {
         if (userUpdateDTO.email() != null) {
             user.setEmail(userUpdateDTO.email());
         }
+
     }
 
-    public UserResDTO detailUser(UUID userId){
+    public UserResDTO detailUser(UUID userId) {
         User user = userRepository.findById(userId).orElseThrow(NoSuchElementException::new);
         return userResMapper.mapModelToDto(user, UserResDTO.class);
+    }
+
+    private void validateLogin(String password, User user){
+        if(!user.getPassword().equals(password)){
+
+        }
+    }
+
+    private void validateEmail(String email) {
+        List<String> emailsSaved = userRepository.findEmails();
+
+        emailsSaved.stream().forEach(existingEMail ->{
+            if (existingEMail.equals(email) ) {
+                throw new EntityExistsException("Email ja está em uso");
+            }});
+    }
+    private void validateCpf(String cpf){
+        List<String> cpfsSaved = userRepository.findCpfs();
+
+        cpfsSaved.stream().forEach(existingCpf ->{
+            if (existingCpf.equals(cpf)) {
+                throw new EntityExistsException("Cpf Já está em uso");
+            }});
+
     }
 }
